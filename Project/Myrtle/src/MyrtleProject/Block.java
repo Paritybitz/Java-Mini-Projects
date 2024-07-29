1b143190779d0c43aeb7cc1a8a1373c3291574b3
Ali Mohsin <ali.mohsin05@gmail.com>
2024-07-29 11:46:40 -0400

    Setup and configs complete.

---
+++ Project/Myrtle/src/MyrtleProject/Block.java
@@ -0,0 +1,59 @@
+package MyrtleProject;
+
+
+
+import java.awt.*;
+public class Block
+{
+    private boolean isWall;
+    private Turtle animal;
+    private int fruitCount;
+
+    public Block(boolean isWall, Turtle animal, int fruitCount){
+        this.isWall = isWall;
+        this.animal = animal;
+        this.fruitCount = fruitCount;
+    }
+
+    public boolean isWall(){ return isWall; }
+    public int getFruitCount(){ return fruitCount; }
+    public void removeFruit(){
+        if(fruitCount > 0){
+            fruitCount--;
+        }
+    }
+
+    public void addFruit(){
+        fruitCount++;
+    }
+    public void addAnimal(Turtle animal){
+        this.animal = animal;
+    }
+    public void removeAnimal(){
+        animal = null;
+    }
+    public boolean hasAnimal(){
+        return animal != null;
+    }
+
+    public void paint(Graphics g, int x, int y, int size){
+        if(isWall){
+            g.setColor(Color.GRAY);
+            g.fillRect(x, y, size, size);
+        }else if(fruitCount > 0){
+            g.setColor(Color.RED);
+            g.fillRect(x, y, size, size);
+            g.setColor(Color.BLACK);
+            String fruitCountString = ""+fruitCount;
+            g.setFont(new Font("Times New Roman", Font.BOLD, 30));
+            g.drawString(fruitCountString, x + (int)(size*.25), y+(int)(size*.9));
+        }
+        int unit = size/10;
+        if(animal != null){
+            animal.paint(g, x, y, size);
+
+
+        }
+    }
+
+}
---
+++ Project/Myrtle/src/MyrtleProject/Myrtle.java
@@ -0,0 +1,100 @@
+package MyrtleProject;
+
+import MyrtleProject.Block;
+import MyrtleProject.Turtle;
+
+
+import javax.swing.*;
+
+public class Myrtle extends Turtle {
+    public Myrtle(JPanel world, Block[][] map, char direction, int row, int col, int fruitCount) {
+        super(world, map, direction, row, col, fruitCount);
+    }
+
+    public void move(int numSteps) {
+        for (int n = 1; n <= numSteps; n++) {
+            move();
+        }
+    }
+
+    public void moveToWall() {
+        while (frontIsClear()) {
+            move();
+        }
+    }
+
+    public void moveToFruit() {
+        while (frontIsClear()) {
+            move();
+            if (isNextToFruit())
+                pickAllFruit();
+        }
+    }
+
+
+    public void pickFruit(int numFruit) {
+        for (int n = 1; n <= numFruit; n++) {
+            pickFruit();
+        }
+    }
+
+    public void pickAllFruit() {
+        while (isNextToFruit()) {
+            pickFruit();
+        }
+    }
+
+    public void placeRow(int numSteps) {
+        for (int steps = 1; steps <= numSteps; steps += 1) {
+            move();
+            placeFruit();
+        }
+    }
+
+    public void findFruitInMaze() {
+        moveToWall();
+        turnLeft();
+        moveToWall();
+        turnLeft();
+        moveToWall();
+        turnRight();
+        moveToWall();
+        turnRight();
+        moveToWall();
+        turnLeft();
+        move(1);
+        turnRight();
+        moveToWall();
+        turnRight();
+        moveToWall();
+        turnRight();
+        moveToWall();
+        turnLeft();
+        moveToWall();
+        turnLeft();
+        moveToWall();
+        turnRight();
+        moveToWall();
+        turnLeft();
+        move(3);
+        turnLeft();
+        moveToWall();
+        turnRight();
+        moveToWall();
+        turnLeft();
+        moveToWall();
+        turnLeft();
+        moveToWall();
+        turnRight();
+        moveToWall();
+        turnRight();
+        moveToWall();
+        turnRight();
+        moveToWall();
+        turnLeft();
+        moveToWall();
+        pickFruit();
+    }
+
+}
+
